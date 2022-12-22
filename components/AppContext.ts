ef210e28a31848b3f7a3e50159f28e2ccb76e75f
iamsalmandev <dev.iamsalman@gmail.com>
2022-12-22 15:39:46 +0500

    removes asset actions on positions page

--- components/AppContext.ts
+++ components/AppContext.ts
@@ -1044,7 +1044,7 @@ export function setupAppContext() {
       ilkToToken$,
       {
         borrow: supportedBorrowIlks,
-        multiply: supportedMultiplyIlks,
+        // multiply: supportedMultiplyIlks,
         earn: supportedEarnIlks,
       },
       uiChanges,
