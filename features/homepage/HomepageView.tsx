f5eb2eb927bb51c2750481c610c28f7799c1b3c6
iamsalmandev <dev.iamsalman@gmail.com>
2022-12-23 11:40:24 +0500

    updates the gradient for info cards

--- features/homepage/HomepageView.tsx
+++ features/homepage/HomepageView.tsx
@@ -344,7 +344,7 @@ export function HomepageView() {
                 text: t('landing.info-cards.learn.key-concepts'),
               },
             ]}
-            backgroundGradient="linear-gradient(127.5deg, #EEE1F9 0%, #FFECE8 56.77%, #DDFFF7 100%)"
+            backgroundGradient="linear-gradient(48deg, rgba(179,95,255,0.25) 0%, rgba(233,74,116,0.25) 100%)"
             backgroundImage=""
           />
           <InfoCard
@@ -368,7 +368,7 @@ export function HomepageView() {
                 text: t('landing.info-cards.support.twitter'),
               },
             ]}
-            backgroundGradient="linear-gradient(135.35deg, #FEF7FF 0.6%, #FEE9EF 100%), radial-gradient(261.45% 254.85% at 3.41% 2.19%, #FFFADD 0%, #FFFBE3 0.01%, #F0FFF2 52.6%, #FBEDFD 100%)"
+            backgroundGradient="linear-gradient(-63deg, rgba(233,74,116,0.2) 0%, rgba(179,95,255,0.2) 100%)"
             backgroundImage=""
           />
         </Grid>
@@ -425,7 +425,7 @@ export function HomepageView() {
                 text: t('landing.info-cards.borrow.open-vault'),
               },
             ]}
-            backgroundGradient="linear-gradient(98.21deg, #FFFBE8 2.63%, #FFF0E8 99.63%), linear-gradient(127.5deg, #E4F9C9 0%, #E8FFF5 49.48%, #F9E1EB 100%)"
+            backgroundGradient="linear-gradient(-63deg, rgba(233,74,116,0.2) 0%, rgba(179,95,255,0.2) 100%)"
             backgroundImage=""
           />
           <InfoCard
@@ -440,7 +440,7 @@ export function HomepageView() {
                 text: t('landing.info-cards.manage.connect-your-wallet'),
               },
             ]}
-            backgroundGradient="linear-gradient(127.5deg, #E8EAFF 0%, #EEF0FF 0%, #FFF3FA 100%), linear-gradient(127.5deg, #DDFFF7 0%, #E8EAFF 61.98%, #F9E1EF 100%)"
+            backgroundGradient="linear-gradient(-63deg, rgba(179,202,101,0.2) 0%, rgba(233,74,116,0.2) 100%)"
             backgroundImage=""
           />
         </Grid>
